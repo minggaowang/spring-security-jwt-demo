60d602edab38e7fb64aa1eaffa921db2805c771e
minggaowang <2209633188@qq.com>
2020-02-23 21:56:41 +0800

    Initial commit

---
+++ src/main/java/com/wmg/springsecurityjwtdemo/HelloController.java
@@ -0,0 +1,51 @@
+package com.wmg.springsecurityjwtdemo;
+
+import com.wmg.springsecurityjwtdemo.models.AuthenticationRequest;
+import com.wmg.springsecurityjwtdemo.models.AuthenticationResponse;
+import com.wmg.springsecurityjwtdemo.service.MyUserDetailsService;
+import com.wmg.springsecurityjwtdemo.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "hello world";
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrent username or password", e);
+        }
+        final UserDetails userDetails = myUserDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+}
---
+++ src/test/java/com/wmg/springsecurityjwtdemo/SpringSecurityJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmg.springsecurityjwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
